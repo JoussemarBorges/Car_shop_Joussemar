75bd53db4c5ecaee106ec2d325608fd8fbea82f3
Joussemar Borges <joussemar.borges@gmail.com>
2023-03-25 16:49:58 -0300

    feat: [req3 - testes unitários] Testando se é possível buscar todos os carros

--- tests/unit/Services/carService.test.ts
+++ tests/unit/Services/carService.test.ts
@@ -3,15 +3,27 @@ import sinon from 'sinon';
 import { Model } from 'mongoose';
 // import ICar from '../../../src/Interfaces/ICar';
 // import Car from '../../../src/Domains/Car';
-import { carDataInputs, carDataOutput } from './MockServiceTest/carServiceMocks.Test';
+import { carDataInput, carDataOutput, allCarsMock } from './MockServiceTest/carServiceMocks.Test';
 import CarService from '../../../src/Services/CarService';
 
-describe('Teste o registro d eum carro', function () {
-  it('Deveria registrar um novo carro com sucesso', async function () {
-    sinon.stub(Model, 'create').resolves(carDataOutput);
-
-    const result = await CarService.registerCar(carDataInputs);
-
-    expect(result).to.be.deep.equal(carDataOutput);
+describe('Testes da carService', function () {
+  describe('Teste o registro de um carro', function () {
+    it('Deveria registrar um novo carro com sucesso', async function () {
+      sinon.stub(Model, 'create').resolves(carDataOutput);
+  
+      const result = await CarService.registerCar(carDataInput);
+  
+      expect(result).to.be.deep.equal(carDataOutput);
+    });
+  });
+  
+  describe('Teste da rota /cars e /cars/:id', function () {
+    it('Deveria ser possível retornar todos os carros cadastrados', async function () {
+      sinon.stub(Model, 'find').resolves(allCarsMock);
+  
+      const result = await CarService.getAllCars();
+  
+      expect(result).to.be.deep.equal(allCarsMock);
+    });
   });
 });
